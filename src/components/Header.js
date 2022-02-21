30e8c8ef406fd585aea1f73a5c948ecd25009c29
hamrajk <hamraj_khangura@hotmail.com>
2022-02-21 11:44:39 -0500

    Created setup and implemented SVG character for hangman figure

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <h1>Hangman</h1>
+      <p>Find the secret word - enter a letter to guess!</p>
+    </>
+  );
+};
+
+export default Header;
