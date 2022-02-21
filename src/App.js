dab46486f6a9c70567894abbf7d022124903fcdf
hamrajk <hamraj_khangura@hotmail.com>
2022-02-21 12:11:58 -0500

    Added blank word template for randomly selected word to appear on screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import "./App.css";
 import Header from "./components/Header";
+import Figure from "./components/Figure";
+import WrongLetters from "./components/WrongLetters";
+import Word from "./components/Word";
+import PopUp from "./components/PopUp";
+import Notification from "./components/Notification";
+import { useState } from "react";
+
+const words = ["application", "programming", "interface", "wizard"];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+let playable = true;
+
+const correctLetters = [];
+const wrongLetters = [];
 
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+
   return (
     <>
       <Header />
+      <div className="game-container">
+        <Figure />
+        <WrongLetters />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+      </div>
     </>
   );
 }
