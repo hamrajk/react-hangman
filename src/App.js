53f89a1fa21c30a16c7b1179d094a0d457d828a6
hamrajk <hamraj_khangura@hotmail.com>
2022-02-21 13:12:16 -0500

    Added functionality to show wrong lettersa and add hangman elements on screen

--- src/App.js
+++ src/App.js
@@ -3,30 +3,52 @@ import Header from "./components/Header";
 import Figure from "./components/Figure";
 import WrongLetters from "./components/WrongLetters";
 import Word from "./components/Word";
-import PopUp from "./components/PopUp";
-import Notification from "./components/Notification";
-import { useState } from "react";
+import { showNotification as show } from "./helpers/helpers";
+
+import { useEffect, useState } from "react";
 
 const words = ["application", "programming", "interface", "wizard"];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
-let playable = true;
-
-const correctLetters = [];
-const wrongLetters = [];
-
 function App() {
   const [playable, setPlayable] = useState(true);
   const [correctLetters, setCorrectLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
+
+  useEffect(() => {
+    const handleKeydown = (event) => {
+      const { key, keyCode } = event;
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters((currentLetters) => [...currentLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters((wrongLetters) => [...wrongLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        }
+      }
+    };
+    window.addEventListener("keydown", handleKeydown);
+
+    return () => window.removeEventListener("keydown", handleKeydown);
+  }, [correctLetters, wrongLetters, playable]);
 
   return (
     <>
       <Header />
       <div className="game-container">
-        <Figure />
-        <WrongLetters />
+        <Figure wrongLetters={wrongLetters} />
+        <WrongLetters wrongLetters={wrongLetters} />
         <Word selectedWord={selectedWord} correctLetters={correctLetters} />
       </div>
     </>
